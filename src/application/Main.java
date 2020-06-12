3f4c4e402e261eafb561ee5fce69edd47011aa05
Hugo Andreassa <hugo.andreassa@gmail.com>
2020-06-12 16:23:52 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,29 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			// Instância o MainView no loader
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			// Carrega a MainView que estava no loader para um Parent
+			Parent parent = loader.load();
+			
+			// Carrega o Parent como uma cena
+			Scene mainScene = new Scene(parent);
+			// Set o mainScene como um estagio
+			primaryStage.setScene(mainScene);
+			// Coloca um título no estagio
+			primaryStage.setTitle("Sample JavaFX Application");
+			// Mostra o estagio
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
