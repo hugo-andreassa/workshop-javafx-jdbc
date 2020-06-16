f60b814ef6a46e6f293157ff3e1c3ce53dd4264f
Hugo Andreassa <hugo.andreassa@gmail.com>
2020-06-16 19:56:21 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,16 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DBException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
+	private DepartmentService service;
+	
 	private Department entity;
 
 	@FXML
@@ -31,22 +39,46 @@ public class DepartmentFormController implements Initializable {
 		initializeNodes();
 	}
 
-	public Department getEntity() {
-		return entity;
-	}
-
 	public void setEntity(Department entity) {
 		this.entity = entity;
 	}
 
-	@FXML
-	public void onBtnSaveAction() {
-		System.out.println("onBtnSaveAction");
+	public void setService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@FXML
-	public void onBtnCancelAction() {
-		System.out.println("onBtnCancelAction");
+	public void onBtnSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalArgumentException("Entity was null");
+		}
+		
+		if(service == null) {
+			throw new IllegalArgumentException("Service was null");
+		}
+		
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+			
+		} catch (DBException e) {
+			Alerts.showAlert("Error saving department", null, e.getMessage(), AlertType.ERROR);
+		}		
+	}
+
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtnCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	private void initializeNodes() {
