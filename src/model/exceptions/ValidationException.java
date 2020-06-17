a9cc6877b25c8d0cabfe36b38c320050876b708d
Hugo Andreassa <hugo.andreassa@gmail.com>
2020-06-17 16:47:19 -0300

    Validation exception

---
+++ src/model/exceptions/ValidationException.java
@@ -0,0 +1,24 @@
+package model.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> errors = new HashMap<>(); 
+	
+	public ValidationException(String msg) {
+		super(msg);
+	}
+	
+	public void addError(String textField, String errorMessage) {
+		errors.put(textField, errorMessage);
+	}
+	
+	public Map<String, String> getErrors(){
+		return errors;
+	}
+	
+}
